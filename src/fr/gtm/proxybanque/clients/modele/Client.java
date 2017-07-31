e518d3262b89e5227336b71188b976eff4063048
Elvynia <elvynia@gmail.com>
2017-07-31 16:51:31 +0200

    add state  getter/setter

--- src/fr/gtm/proxybanque/clients/modele/Client.java
+++ src/fr/gtm/proxybanque/clients/modele/Client.java
@@ -1,9 +1,17 @@
 package fr.gtm.proxybanque.clients.modele;
 
 public class Client {
-	
 
 	private String name;
 	private String state;
+	
+	
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	
 
 }
