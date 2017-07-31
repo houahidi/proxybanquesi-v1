d7eca4f3bd525a8cd66774d7b57399acb3295ae2
Elvynia <elvynia@gmail.com>
2017-07-31 16:53:35 +0200

    getter/setter name

--- src/fr/gtm/proxybanque/clients/modele/Client.java
+++ src/fr/gtm/proxybanque/clients/modele/Client.java
@@ -1,9 +1,17 @@
 package fr.gtm.proxybanque.clients.modele;
 
 public class Client {
-	
 
 	private String name;
 	private String state;
+	
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
 
 }
